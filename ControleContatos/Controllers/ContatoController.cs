b37862f84932e9441889454ddfff31208d7b26a3
vedovatto30 <f_vedovatto@yahoo.com.br>
2022-07-16 18:56:04 -0300

    Add project files.

---
+++ ControleContatos/Controllers/ContatoController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControleContatos.Controllers
+{
+    public class ContatoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Criar() //sera criado as actions das paginas dos botoes
+        {
+            return View();
+        }
+        public IActionResult Editar()
+        {
+            return View();
+        }
+
+        public IActionResult ApagarConfirmacao()
+        {
+            return View();
+        }
+    }
+}
